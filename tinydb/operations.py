bffba79bb4f2a94f057f177ba06c617495b249bd
Markus Siemens <markus@m-siemens.de>
2026-05-28 22:57:08 +0200

    fix: use MutableMapping for operations

--- tinydb/operations.py
+++ tinydb/operations.py
@@ -9,64 +9,64 @@ This would delete the ``foo`` field from all documents where ``foo`` equals 2.
 """
 
 
-from typing import Callable, Mapping, Any, Union
+from typing import Callable, MutableMapping, Any, Union
 
 
-def delete(field: str) -> Callable[[Mapping], None]:
+def delete(field: str) -> Callable[[MutableMapping], None]:
     """
     Delete a given field from the document.
     """
-    def transform(doc: Mapping):
+    def transform(doc: MutableMapping):
         del doc[field]
 
     return transform
 
 
-def add(field: str, n: Union[int, float]) -> Callable[[Mapping], None]:
+def add(field: str, n: Union[int, float]) -> Callable[[MutableMapping], None]:
     """
     Add ``n`` to a given field in the document.
     """
-    def transform(doc: Mapping):
+    def transform(doc: MutableMapping):
         doc[field] += n
 
     return transform
 
 
-def subtract(field: str, n: Union[int, float]) -> Callable[[Mapping], None]:
+def subtract(field: str, n: Union[int, float]) -> Callable[[MutableMapping], None]:
     """
     Subtract ``n`` to a given field in the document.
     """
-    def transform(doc: Mapping):
+    def transform(doc: MutableMapping):
         doc[field] -= n
 
     return transform
 
 
-def set(field: str, val: Any) -> Callable[[Mapping], None]:
+def set(field: str, val: Any) -> Callable[[MutableMapping], None]:
     """
     Set a given field to ``val``.
     """
-    def transform(doc: Mapping):
+    def transform(doc: MutableMapping):
         doc[field] = val
 
     return transform
 
 
-def increment(field: str) -> Callable[[Mapping], None]:
+def increment(field: str) -> Callable[[MutableMapping], None]:
     """
     Increment a given field in the document by 1.
     """
-    def transform(doc: Mapping):
+    def transform(doc: MutableMapping):
         doc[field] += 1
 
     return transform
 
 
-def decrement(field: str) -> Callable[[Mapping], None]:
+def decrement(field: str) -> Callable[[MutableMapping], None]:
     """
     Decrement a given field in the document by 1.
     """
-    def transform(doc: Mapping):
+    def transform(doc: MutableMapping):
         doc[field] -= 1
 
     return transform
